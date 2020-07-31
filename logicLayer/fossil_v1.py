c0c97887f080927cb7a89d4c7e11c09703e8ccb2
yixuan_ruan <18569402593@163.com>
2020-07-31 09:33:45 +0800

    Local Fossil Re-segmentation

--- logicLayer/fossil_v1.py
+++ logicLayer/fossil_v1.py
@@ -1,4 +1,9 @@
+from skimage import img_as_float
+from skimage.segmentation import (morphological_chan_vese,
+                                  checkerboard_level_set)
+from skimage.segmentation import clear_border
 from utils import Utils3D
+import numpy as np
 
 class Fossil:
     def __init__(self, binary_voxel, gray_voxel, labeled_id,
@@ -16,9 +21,48 @@ class Fossil:
         self.three_view = None
         self.getThreeView()
 
+        self.reseged_binary_voxel = None
+        self.reseged_three_view = None
+
     def getThreeView(self):
         # [0] left , [1] upper , [2] front
         self.three_view = Utils3D.getThreeViews(self.binary_voxel)
+
+    def reseg(self,iteration):
+        self.reseged_binary_voxel = np.zeros_like(self.gray_voxel)
+        for i in range(self.gray_voxel.shape[0]):
+            self.reseged_binary_voxel[i] = self._MorphACWE(self.gray_voxel[i],iteration)
+        return self.reseged_binary_voxel
+
+    def getNewThreeView(self,iteration):
+        self.reseg(iteration)
+        self.reseged_three_view = Utils3D.getThreeViews(self.reseged_binary_voxel)
+        return self.reseged_three_view,  self.reseged_binary_voxel
+
+    def _MorphACWE(self, img,iteration):
+        image = img_as_float(img)
+
+        # Initial level set
+        init_ls = checkerboard_level_set(image.shape, 6)
+        # List with intermediate results for plotting the evolution
+        evolution = []
+        callback = self._store_evolution_in(evolution)
+        ls = morphological_chan_vese(image, iteration, init_level_set=init_ls, smoothing=3,
+                                     iter_callback=callback)
+        # if np.sum(ls) > (ls.shape[0] * ls.shape[1] / 2):
+        #     ls = 1 - ls
+        ls = clear_border(ls)
+        return ls
+
+    def _store_evolution_in(self,lst):
+        """Returns a callback function to store the evolution of the level sets in
+        the given list.
+        """
+
+        def _store(x):
+            lst.append(np.copy(x))
+
+        return _store
 
 if __name__ == '__main__':
     import numpy as np
